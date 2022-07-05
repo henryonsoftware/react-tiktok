dae7d14f857c7d8842812860eed1db1977e40c87
Henry Bui <viethuongdev@gmail.com>
2022-07-05 08:43:51 +0700

    Create env file and move the variables out

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpRequest = axios.create({
-  baseURL: 'https://tiktok.fullstack.edu.vn/api/',
+  baseURL: process.env.REACT_APP_BASE_URL,
   timeout: 1000,
 })
 

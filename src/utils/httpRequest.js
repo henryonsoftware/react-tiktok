8619be82c84434852b0bd05487e04f516e98ca3c
Henry Bui <viethuongdev@gmail.com>
2022-07-05 08:35:30 +0700

    Rename the utils file

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-const request = axios.create({
+const httpRequest = axios.create({
   baseURL: 'https://tiktok.fullstack.edu.vn/api/',
   timeout: 1000,
 })
 
 export const get = async (uri, options = {}) => {
-  const response = await request.get(uri, options)
+  const response = await httpRequest.get(uri, options)
   return response.data
 }
 
-export default request
+export default httpRequest

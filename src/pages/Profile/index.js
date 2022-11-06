60e70b38dc2ec94cdd81428d52a752af76d824ae
Henry Bui <viethuongdev@gmail.com>
2022-11-07 01:10:23 +0700

    Handle follow and unfollow on profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,29 +1,29 @@
-import { LockIcon } from '~/components/Icons'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Tippy from '@tippyjs/react'
 import * as userService from '~/services/userService'
-import { FollowedIcon } from '~/components/Icons'
+import { LockIcon, FollowedIcon } from '~/components/Icons'
 
 function Profile() {
   const { nickname } = useParams()
   const [user, setUser] = useState(null)
+  const [followed, setFollowed] = useState(false)
+  const currentUser = JSON.parse(localStorage.getItem('user'))
+  const accessToken = currentUser && currentUser.meta.token ? currentUser.meta.token : ''
 
   useEffect(() => {
     if (nickname) {
-      const currentUser = JSON.parse(localStorage.getItem('user'))
-      const accessToken = currentUser && currentUser.meta.token ? currentUser.meta.token : ''
-
       userService
         .getUserProfile({ nickname, accessToken })
         .then((data) => {
           setUser(data)
+          setFollowed(data.is_followed)
         })
         .catch((error) => {
           console.log(error)
         })
     }
-  }, [nickname])
+  }, [nickname, accessToken])
 
   function handleOnMouseOver(e) {
     e.target.play()
@@ -33,10 +33,46 @@ function Profile() {
     e.target.pause()
   }
 
+  function handleFollow() {
+    if (!accessToken) {
+      alert('Please login!')
+      return
+    }
+
+    userService
+      .followAnUser({ userId: user.id, accessToken: accessToken })
+      .then((res) => {
+        if (res.data) {
+          setFollowed(res.data.is_followed)
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  function handleUnfollow() {
+    if (!accessToken) {
+      alert('Please login!')
+      return
+    }
+
+    userService
+      .unfollowAnUser({ userId: user.id, accessToken: currentUser.meta.token })
+      .then((res) => {
+        if (res.data) {
+          setFollowed(res.data.is_followed)
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return user ? (
     <div className="py-6 px-4">
       <div className="flex items-start">
-        <div className="flex items-center mr-6 cursor-pointer">
+        <div className="flex items-center mr-3 md:mr-6 cursor-pointer">
           <img
             src={user.avatar}
             alt={user.nickname}
@@ -46,25 +82,33 @@ function Profile() {
         <div className="flex flex-col">
           <div className="font-bold text-xl md:text-2xl lg:text-3xl cursor-pointer mb-2">{user.nickname}</div>
           <div className="font-semibold text-base lg:text-lg cursor-pointer">{`${user.first_name} ${user.last_name}`}</div>
-          <div className="flex items-center mt-4">
-            <button
-              className={`text-lg font-semibold rounded-md cursor-pointer py-1 px-3 w-44 ${
-                user.is_followed
-                  ? 'bg-white text-primary border border-solid border-primary hover:bg-secondary'
-                  : 'bg-primary text-white'
-              }`}
-            >
-              {user.is_followed ? 'Message' : 'Follow'}
-            </button>
-            <Tippy content="Unfollow">
-              <button className="flex items-center justify-center ml-2 rounded border border-solid border-black/30 p-2 cursor-pointer bg-transparent text-black/80">
-                <FollowedIcon width={20} height={20} />
+          <div className="flex items-center justify-end md:justify-start mt-4">
+            {followed ? (
+              <>
+                <button className="text-lg font-semibold rounded-md cursor-pointer py-1 px-3 w-44 h-10 bg-white text-primary border border-solid border-primary hover:bg-secondary">
+                  Message
+                </button>
+                <Tippy content="Unfollow" placement="bottom">
+                  <button
+                    className="flex items-center justify-center ml-2 rounded border border-solid border-black/30 w-10 h-10 cursor-pointer bg-transparent text-black/80"
+                    onClick={handleUnfollow}
+                  >
+                    <FollowedIcon width={20} height={20} />
+                  </button>
+                </Tippy>
+              </>
+            ) : (
+              <button
+                className="text-lg font-semibold rounded-md cursor-pointer py-1 px-3 w-44 h-10 bg-primary text-white"
+                onClick={handleFollow}
+              >
+                Follow
               </button>
-            </Tippy>
+            )}
           </div>
         </div>
       </div>
-      <div className="flex flex-row items-center my-6">
+      <div className="flex flex-row items-center justify-between sm:justify-start my-6">
         <div className="flex items-center text-black/50 mr-4">
           <span className="font-secondary font-bold text-black mr-2 text-lg">{user.followings_count}</span>
           <span className="font-primary text-base">Following</span>

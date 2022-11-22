9b9b1d8ae9b0db9229209983980e32f1e9f1af3d
Henry Bui <viethuongdev@gmail.com>
2022-11-22 22:48:43 +0700

    Implement infinity load

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -6,7 +6,7 @@ function DefaultLayout({ children }) {
   return (
     <div className="flex flex-col items-center">
       <Header />
-      <div className="max-w-full flex justify-between py-0 px-0 md:px-6 mt-16" style={{ width: '1150px' }}>
+      <div className="max-w-full py-0 px-0 md:px-6 mt-16" style={{ width: '1150px' }}>
         <div>
           <Sidebar />
         </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
-import { useContext } from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext, useCallback } from 'react'
+import { Virtuoso } from 'react-virtuoso'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { AuthUserContext } from '~/App'
 import Video from '~/layouts/components/Video'
 import * as timelineService from '~/services/timelineService'
@@ -13,29 +15,59 @@ function HomePage() {
   const authUser = useContext(AuthUserContext)
   const accessToken = authUser && authUser.meta.token ? authUser.meta.token : ''
 
-  useEffect(() => {
-    timelineService
-      .getVideos({ type: 'for-you', page: page, accessToken: accessToken })
-      .then((res) => {
-        if (Array.isArray(res.data)) {
-          setVideos((prev) => [...prev, ...res.data])
-        }
+  const loadMore = useCallback(() => {
+    return setTimeout(() => {
+      timelineService
+        .getVideos({ type: 'for-you', page: page, accessToken: accessToken })
+        .then((res) => {
+          if (Array.isArray(res.data)) {
+            setVideos((prev) => [...prev, ...res.data])
+            setPage((prev) => prev + 1)
+          }
 
-        if (res.data.length === 0 || page === res.meta.pagination.total) {
-          setNoMoreVideo(true)
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }, [page, accessToken])
+          if (res.data.length === 0 || page === res.meta.pagination.total) {
+            setNoMoreVideo(true)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }, 300)
+  }, [page, accessToken, setVideos])
+
+  useEffect(() => {
+    const timeout = loadMore()
+    return () => clearTimeout(timeout)
+  }, [])
 
   return (
     <div className="mx-4 py-8">
-      {videos.map((video) => (
-        <Video key={video.id} video={video} />
-      ))}
-      {noMoreVideo && <p className="text-center">No more video to load</p>}
+      <Virtuoso
+        data={videos}
+        useWindowScroll
+        endReached={() => {
+          if (!noMoreVideo) {
+            loadMore()
+          }
+        }}
+        itemContent={(index, video) => <Video key={index} video={video} />}
+        components={{
+          Footer: () => {
+            return (
+              <div className="flex justify-center p-8">
+                {noMoreVideo ? (
+                  <p>No more video</p>
+                ) : (
+                  <FontAwesomeIcon
+                    className="text-black/30 text-sm bg-transparent cursor-pointer animate-spin w-4 h-4 md:w-6 md:h-6"
+                    icon={faSpinner}
+                  />
+                )}
+              </div>
+            )
+          },
+        }}
+      />
     </div>
   )
 }
